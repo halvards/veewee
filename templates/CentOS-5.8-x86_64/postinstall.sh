81ab6a8242ff966d23cff81c729b966944d66378
Halvard Skogsrud <hskogsru@thoughtworks.com>
2012-05-01 22:59:48 +1000

    Changes to CentOS 5.8 x64 template to install Ruby and Puppet via RPM

--- templates/CentOS-5.8-x86_64/postinstall.sh
+++ templates/CentOS-5.8-x86_64/postinstall.sh
@@ -1,5 +1,3 @@
-#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
-
 date > /etc/vagrant_box_build_time
 
 fail()
@@ -8,53 +6,79 @@ fail()
   exit 1
 }
 
-#kernel source is needed for vbox additions
-yum -y install gcc bzip2 make kernel-devel-`uname -r`
-#yum -y update
-#yum -y upgrade
+yum -y erase avahi bitstream-vera-fonts freetype gtk2 hicolor-icon-theme libX11 wireless-tools *i386 *i586 *i686
+yum -y install bzip2 gcc kernel-devel-`uname -r` make yum-priorities
+yum -y --exclude=kernel* update
 
-yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y clean all
+cat > /etc/yum.repos.d/puppetlabs.repo << EOM
+[puppetlabs]
+name=puppetlabs
+baseurl=http://yum.puppetlabs.com/el/5/products/\$basearch
+enabled=1
+gpgcheck=0
+EOM
 
-#Installing ruby
-cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz || fail "Could not download Ruby source"
-tar xzvf ruby-1.9.2-p180.tar.gz 
-cd ruby-1.9.2-p180
-./configure
-make && make install
-cd /tmp
-rm -rf /tmp/ruby-1.9.2-p180
-rm /tmp/ruby-1.9.2-p180.tar.gz
-ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path
-ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/5/\$basearch
+enabled=1
+gpgcheck=0
+EOM
 
-#Installing chef & Puppet
-echo "Installing chef and puppet"
-/usr/local/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
-/usr/local/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
+cat > /etc/yum.repos.d/rbel.repo << EOM
+[rbel]
+name=rbel
+baseurl=http://rbel.frameos.org/stable/el5/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install facter puppet ruby-devel.x86_64 rubygems || fail "Could not install puppet"
+#yum -y clean all
+rm /etc/yum.repos.d/{puppetlabs,epel,rbel}.repo
+
+#gem install --no-ri --no-rdoc chef || fail "Could not install chef"
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+#VBOX_VERSION='4.1.14'
 cd /tmp
 wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+# Remove RPMs no longer required
+yum -y erase cpp gcc gcc-c++ kernel-devel-`uname -r` make
+yum -y clean all
+
+# Disable grub boot timeout
+sed -i "s/timeout=5/timeout=0/" /boot/grub/grub.conf
+sed -i "s/timeout=5/timeout=0/" /boot/grub/menu.lst
+
+# Disable IPv6 because Vagrant does not like it
+sed -i "s/NETWORKING_IPV6=yes/NETWORKING_IPV6=no/" /etc/sysconfig/network
+echo "NETWORKING_IPV6=no" >> /etc/sysconfig/network
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+/sbin/chkconfig ip6tables off
+
+# Restart NetworkManager on boot to consistently get DHCP assigned IP address
+echo "" >> /etc/rc.local
+echo "/sbin/udevadm trigger" >> /etc/rc.local
+echo "/sbin/service NetworkManager restart" >> /etc/rc.local
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 sed -i "s/^\(.*env_keep = \"\)/\1PATH /" /etc/sudoers
 
-#poweroff -h
+dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
 
 exit
